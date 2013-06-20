a0276e55f7fe1283e30c919bcecbe3eb9cca4530
gitmac <gitmac@qq.com>
2013-06-20 10:46:12 +0800

    [Add] Demo

---
+++ Demo/Demo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by gitmac on 13-6-20.
+//  Copyright (c) 2013年 gitmac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Demo/Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Demo
+//
+//  Created by gitmac on 13-6-20.
+//  Copyright (c) 2013年 gitmac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
